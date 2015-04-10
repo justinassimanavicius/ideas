613266b9409f45a6741b763bbfaa599e8abf3785
Igor Gubaidulin <i@igor.lt>
2015-04-10 22:45:34 +0300

    Removed jQuery dependency, changed mocks. Small design changes

--- IdeasAPI/app/mock/items.js
+++ IdeasAPI/app/mock/items.js
@@ -1,7 +1,7 @@
 ﻿[
 {
 	"title": "New simple WYSIWYG content editor with column support",
-	"message": "New simple WYSIWYG content editor with column support New simple WYSIWYG content editor with column support Auto-Generate Facebook-style Link Preview when inserting URL into Editor",
+	"message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend, mauris nec vestibulum tincidunt, diam mi lobortis lectus, vitae fringilla nisi magna sed nulla. Proin vestibulum sapien sed neque auctor, sed venenatis augue ultricies. Phasellus tincidunt, enim et volutpat iaculis, lacus ligula tincidunt nibh, eget venenatis nulla libero ultrices purus. Morbi in lacus ac magna consequat fringilla. Cras convallis, velit non porttitor varius, erat urna cursus risus, et rhoncus lectus enim nec metus. Nulla facilisi. Morbi pretium justo metus, sit amet tincidunt eros ultrices nec. Praesent efficitur mattis neque, eu molestie elit dapibus nec. Phasellus consectetur, nisi ut imperdiet hendrerit, risus ex feugiat ex, in posuere arcu sem non neque. Vestibulum vestibulum lorem quis felis mattis, ultricies porta enim egestas.",
 	"id": 1,
 	"vote": 5,
 	"author": "Joe Jackson",
@@ -10,9 +10,9 @@
 },
 {
 	"title" : "Auto-Generate Facebook-style Link Preview when inserting URL into Editor",
-	"message": "New simple WYSIWYG content editor with column support New simple WYSIWYG content editor with column support Auto-Generate Facebook-style Link Preview when inserting URL into Editor",
+	"message": "Aenean scelerisque imperdiet massa. Cras urna metus, sagittis sed ornare quis, egestas non leo. Nulla mollis at nisl eu iaculis. Maecenas commodo nisl nec tortor suscipit cursus. Mauris nec suscipit odio. Donec vel convallis ante. Sed vitae mollis mauris, id egestas ligula. Aliquam commodo vel neque eu iaculis. Nulla euismod fringilla diam et malesuada. Aliquam aliquam quis leo vitae maximus. Sed ullamcorper ligula at leo fringilla euismod. Morbi vitae nisl elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In faucibus rhoncus lectus sed fermentum. Nulla eu gravida libero. Nulla facilisi.",
 	"id": 2,
-	"vote": 11,
+	"vote": 1101,
 	"author": "Jonas Jankauskas",
 	"status": "Pending",
 	"comments": 5
