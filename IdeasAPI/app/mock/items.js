304cb4479f41d5a6e5c2a38bae844a35613e0b74
Igor Gubaidulin <i@igor.lt>
2015-04-10 17:45:38 +0300

    Updated mocks, changed some styles and HTML

--- IdeasAPI/app/mock/items.js
+++ IdeasAPI/app/mock/items.js
@@ -1,10 +1,18 @@
 ﻿[
 {
-	"message": "idea 1",
-	"id":1
+	"message": "New simple WYSIWYG content editor with column support",
+	"id": 1,
+	"vote": 5,
+	"author": "Joe Jackson",
+	"status": "Pending",
+	"comments": 5
 },
 {
-	"message": "idea 2",
-	"id":2
+	"message": "Auto-Generate Facebook-style Link Preview when inserting URL into Editor",
+	"id": 2,
+	"vote": 11,
+	"author": "Shonda Rhymes",
+	"status": "Pending",
+	"comments": 5
 }
 ]
