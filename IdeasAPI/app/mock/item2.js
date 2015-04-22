a01d7ca8fc8d248a2ae18191eaa01c179b4217aa
Igor Gubaidulin <igor.gubaidulin@nortal.com>
2015-04-22 17:53:55 +0300

    Item view design

--- IdeasAPI/app/mock/item2.js
+++ IdeasAPI/app/mock/item2.js
@@ -1,4 +1,12 @@
 ﻿{
-	"message":"idea 2",
-	"id":2
+	"id":2,
+	"createDate": "1900-01-01T00:00:00",
+  	"updateDate": null,
+  	"vote": 54,
+  	"title": "New simple WYSIWYG content editor with column support",
+	"message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eleifend, mauris nec vestibulum tincidunt, diam mi lobortis lectus, vitae fringilla nisi magna sed nulla. Proin vestibulum sapien sed neque auctor, sed venenatis augue ultricies. Phasellus tincidunt, enim et volutpat iaculis, lacus ligula tincidunt nibh, eget venenatis nulla libero ultrices purus. Morbi in lacus ac magna consequat fringilla. Cras convallis, velit non porttitor varius, erat urna cursus risus, et rhoncus lectus enim nec metus. Nulla facilisi. Morbi pretium justo metus, sit amet tincidunt eros ultrices nec. Praesent efficitur mattis neque, eu molestie elit dapibus nec. Phasellus consectetur, nisi ut imperdiet hendrerit, risus ex feugiat ex, in posuere arcu sem non neque. Vestibulum vestibulum lorem quis felis mattis, ultricies porta enim egestas.",
+	"author": "Jonas Jankauskas",
+	"status": "Pending",
+	"comments": 5
 }
+
